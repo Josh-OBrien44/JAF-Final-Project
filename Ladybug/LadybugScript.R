1c150abea75f33beeab5949ab7b4bfb0648775fb
flynnhartigan <98784364+flynnhartigan@users.noreply.github.com>
2022-05-05 20:46:08 -0500

    Add files via upload

---
+++ Ladybug/LadybugScript.R
@@ -0,0 +1,13 @@
+###DATA 332-01 FInal Project Script Library
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(lubridate)
+library(readr)
+
+rm(list = ls())
+setwd("C:/Users/flynn/OneDrive/Desktop/Augie Junior Year/DATA 332-01")
+
+
+df_ladybug <- read_excel("FinalProject/Ladybug Data.xlsx", sheet = 1)
+df_scanladybug <- read_excel("FinalProject/Scan Ladybug Data.xlsx", sheet = 1)
